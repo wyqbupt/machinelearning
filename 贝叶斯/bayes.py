a80964ee4dfec6111698fbcc9f97b3a3df479efd
wyqbupt <wyqcsnet@yeah.net>
2015-04-20 21:28:17 +0800

    complete a naive bayes

--- 贝叶斯/bayes.py
+++ 贝叶斯/bayes.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from numpy import *
+import re
 def loadDataSet():
     postingList=[['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                  ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
@@ -71,3 +72,42 @@ def bagOfWords2VecMN(vocabList,inputSet):
 	    returnVec[vocabList.index(word)] += 1
     return returnVec
 
+def textParse(bigString):
+    listOfTokens = re.split(r'\W*',bigString)
+    return [tok for tok in listOfTokens if len(tok)>2]
+
+def spamTest():
+    docList=[]
+    classList = []
+    fullText = []
+    for i in range(1,26):
+	wordList = textParse(open('email/spam/%d.txt' % i).read())
+	docList.append(wordList)
+	fullText.extend(wordList)
+	classList.append(1)
+	wordList = textParse(open('email/ham/%d.txt' % i).read())
+	docList.append(wordList)
+	fullText.extend(wordList)
+	classList.append(0)
+    vocabList = createVocabList(docList)
+    trainingSet = range(50)
+    testSet = []
+    for i in range(10):
+	randIndex = int(random.uniform(0,len(trainingSet)))
+	testSet.append(trainingSet[randIndex])
+	del(trainingSet[randIndex]) #the size of test set if 10 ,the rest is training set
+    trainMat=[]
+    trainClasses = []
+    for docIndex in trainingSet:
+	trainMat.append(bagOfWords2VecMN(vocabList, docList[docIndex]))
+	trainClasses.append(classList[docIndex])
+    p0V,p1V,pSpam = trainNB0(array(trainMat),array(trainClasses))
+    errorCount = 0
+    for docIndex in testSet:
+	wordVector = setOfWords2Vec(vocabList,docList[docIndex])
+	if classifyNB(array(wordVector),p0V,p1V,pSpam) != classList[docIndex]:
+	    errorCount += 1
+    print 'the error number is: ',errorCount
+    print 'the error rate is: ',float(errorCount)/len(testSet)
+	    
+spamTest()
